a8234252e88007cf119d7daf340b407142ca92b6
FDoerr <183077623+FDoerr@users.noreply.github.com>
2024-11-14 15:32:05 +0100

    wip construct_new_name

--- src/renamer/renamer.py
+++ src/renamer/renamer.py
@@ -17,11 +17,20 @@ def get_file_names():
 def rename_files(file_names):
     for file_name in file_names:
         new_name = construct_new_name(file_name)
-        shutil.copyfile(f'{source_folder_path}/{file_name}', f'{target_folder_path}/{new_name}')
+        print(new_name)
+        #shutil.copyfile(f'{source_folder_path}/{file_name}', f'{target_folder_path}/{new_name}')
 
 
 def construct_new_name(file_name:str):
+    identifier =file_name[:-18] # e.g. NOtizen Berichtsheft  or Berichtsheft
+    start_date: str = file_name[-17:-11]
+    end_date: str = file_name[-10:-4]
+    print(f'{identifier=}')
+    print(f'{start_date=}')
+    print(f'{end_date=}')    
     ...
+    #extract date
+    #convert date
     #return new_name
 
 
