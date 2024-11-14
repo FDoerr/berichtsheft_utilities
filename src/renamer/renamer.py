91a5d572116ff88acdec9ac375db8f45843e7775
FDoerr <183077623+FDoerr@users.noreply.github.com>
2024-11-14 13:05:27 +0100

    setup file structure

---
+++ src/renamer/renamer.py
@@ -0,0 +1,7 @@
+import sys
+import datetime
+
+'''
+rename reports from: "Notizen Berichtsheft ddMMYY_ddMMYY" to "KWUU_YY_MM_dd" U is the calenderweek
+string format codes: https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+'''
