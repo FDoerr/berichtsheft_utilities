437dc2d80a1831355ea431faf17f022ba0bf769e
FDoerr <183077623+FDoerr@users.noreply.github.com>
2024-11-15 15:44:23 +0100

    formatting

--- src/renamer/renamer.py
+++ src/renamer/renamer.py
@@ -29,29 +29,31 @@ def rename_files(file_names: list[str]) -> None:
 
 
 def construct_new_name(file_name: str) -> str:  
+    # this could probably be done with a RegEx
     #   4321098765432109876543210987654321       <--counting digit index backwards
     #   Notizen Berichtsheft ddmmyy_ddmmyy.txt
     #           Berichtsheft ddmmyy_ddmmyy.txt
-    file_extension: str =  fetch_file_extension(file_name)
-    len_fe: int= len(file_extension) #short name for better readability
-
-    identifier: str = file_name[:-14-len_fe].replace(' ','_') # e.g. Notizen Berichtsheft  or Berichtsheft    
     
-    start_date: str = file_name[-13-len_fe:-7-len_fe]    
+    file_extension: str =  fetch_file_extension(file_name)
+    len_ext: int= len(file_extension) # short name for better readability
+
+    identifier: str = file_name[:-14-len_ext].replace(' ','_') # e.g. Notizen Berichtsheft  or Berichtsheft    
+    
+    start_date: str = file_name[-13-len_ext:-7-len_ext]    
     new_start_date_str: str | None = convert_date(start_date, True)
 
-    end_date: str = file_name[-6-len_fe:-len_fe]
-    new_end_date_str: str | None= convert_date(end_date)    
+    end_date: str = file_name[-6-len_ext:-len_ext]
+    new_end_date_str: str | None = convert_date(end_date)    
 
-    new_name: str = f'{new_start_date_str}-{new_end_date_str}_{identifier}{file_extension}'
+    new_name: str = f'{new_start_date_str}-{new_end_date_str}_{identifier}{file_extension}' # e. g. 2024-KW44_0411-0811_Notizen_Berichtsheft.txt
     return new_name
     
 
-def fetch_file_extension(file_name):
+def fetch_file_extension(file_name) -> str | None:
     try:
-        pattern:re.Pattern = r"(\.\w+)$" # parenthesis group expressions, \. is "."  \w is letters, numbers, and underscore, + one or more of the preceding token, $ end of string
-        result = re.search(pattern, file_name)
-        file_extension = result.group(1)
+        pattern:re.Pattern      = r"(\.\w+)$" # parenthesis group expressions, \. is "."  \w is letters, numbers, and underscore, + one or more of the preceding token, $ end of string
+        result:re.Match         = re.search(pattern, file_name)
+        file_extension:str|None = result.group(1)
         return file_extension
     except AttributeError as e:
         print(f'No matching pattern found.\nDoes the file have an extension?\n Error: {e}')
@@ -67,7 +69,7 @@ def convert_date(old_date_str: str, is_start_date: bool|None = None) -> str | No
             new_date_format: str = '%d%m'
         
         old_date: datetime = datetime.strptime(old_date_str, old_date_format)
-        new_date_str = datetime.strftime(old_date, new_date_format)
+        new_date_str:str   = datetime.strftime(old_date,     new_date_format)
         return(new_date_str)
     except:
         raise ValueError('unexpected Name. is the Name in the Format: "Name ddmmyy_ddmmyy.txt" ?')
